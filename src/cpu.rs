b5b993984bcc3dc183f05bf4fb8a5122f9db0010
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-06-18 23:49:53 +0100

    App: Create window, update name

--- src/cpu.rs
+++
@@ -1,7 +0,0 @@
-use crate::memory::memory;
-
-pub struct CPU {
-    pub regs: [u32; 16], // Array of general-purpose registers
-    pub pc: u32, // Program counter
-    pub cpsr: u32, // Current program status register
-}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,40 @@
-fn main() {
-// Initialize components
-    println!("Hello, world!");
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::egui;
+
+fn main() -> Result<(), eframe::Error> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(800.0, 600.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Prometheus",
+        options,
+        Box::new(|_cc| Box::<MyApp>::default()),
+    )
+}
+
+struct MyApp {
+    input_buffer: String,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            input_buffer: "".to_owned(),
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                let name_label = ui.label(">");
+                ui.text_edit_singleline(&mut self.input_buffer)
+                    .labelled_by(name_label.id);
+            });
+        });
+    }
 }
