422f2abe7d17c8defb897887cfb6510650f023b2
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-06-19 21:05:47 +0100

    App: Add basic ls cmd

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use std::process::Command;
 use eframe::egui;
 
 fn main() -> Result<(), eframe::Error> {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    env_logger::init();
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(800.0, 600.0)),
         ..Default::default()
@@ -11,30 +11,55 @@ fn main() -> Result<(), eframe::Error> {
     eframe::run_native(
         "Prometheus",
         options,
-        Box::new(|_cc| Box::<MyApp>::default()),
+        Box::new(|_cc| Box::<Prometheus>::default()),
     )
 }
 
-struct MyApp {
+struct Prometheus {
     input_buffer: String,
+    command_output: String,
 }
 
-impl Default for MyApp {
+impl Default for Prometheus {
     fn default() -> Self {
         Self {
-            input_buffer: "".to_owned(),
+            input_buffer: String::new(),
+            command_output: String::new()
         }
     }
 }
 
-impl eframe::App for MyApp {
+impl eframe::App for Prometheus {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
                 let name_label = ui.label(">");
                 ui.text_edit_singleline(&mut self.input_buffer)
                     .labelled_by(name_label.id);
+
+                if ui.button("Execute").clicked() {
+                    if let Ok(output) = Command::new("sh")
+                        .arg("-c")
+                        .arg(&self.input_buffer)
+                        .output()
+                    {
+                        if output.status.success() {
+                            if let Ok(stdout) = String::from_utf8(output.stdout) {
+                                self.command_output = stdout;
+                            }
+                        } else {
+                            if let Ok(stderr) = String::from_utf8(output.stderr) {
+                                self.command_output = stderr;
+                            }
+                        }
+                    } else {
+                        self.command_output = String::from("Failed to execute command");
+                    }
+                }
+
+                ui.label(&self.command_output);
             });
         });
     }
 }
+
